1c6503ea9a8274c9bd9472115d21057511707af1
Rostik <Rostik@DESKTOP-I9ORA34>
2023-11-18 22:21:33 +0200

    Code readability improved

--- Lab3Multithreading/Lab3Multithreading.cpp
+++ Lab3Multithreading/Lab3Multithreading.cpp
@@ -34,19 +34,13 @@ int main(int argc, char* argv[])
     std::vector<std::pair<char, int>> tasks5 {{'a', 5}, {'b', 2}, {'c', 2}, {'d', 2}, {'e', 2}, {'f', 2}, {'f', 7}, {'h', 1}, {'i', 2}, {'j', 2}, {'j', 7} };
     
     std::cout << "Execution started" << std::endl;
-    
-    std::jthread t1(threads_sync::t, tasks1);
-    std::jthread t2(threads_sync::t, tasks2);
-    std::jthread t3(threads_sync::t, tasks3);
-    std::jthread t4(threads_sync::t, tasks4);
-    std::jthread t5(threads_sync::t, tasks5);
-
-    // Joining manually because it is necessary to display "Execution ended" when threads are done their work
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    t5.join();
+    {
+        std::jthread t1(threads_sync::t, tasks1);
+        std::jthread t2(threads_sync::t, tasks2);
+        std::jthread t3(threads_sync::t, tasks3);
+        std::jthread t4(threads_sync::t, tasks4);
+        std::jthread t5(threads_sync::t, tasks5);
+    }
     
     std::cout << "Execution ended" << std::endl;
     return 0;
